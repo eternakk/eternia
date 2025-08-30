ec65814f228f5b2d1142fd34de8c04cebff1242d
Klajdi Kolaj <kljdkolaj@gmail.com>
2025-08-30 21:54:05 +0200

    Expand ESLint ignore list to include build, test, and coverage directories.

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'dist-analyze', 'node_modules', 'coverage', '.cr-out', 'storybook-static', 'cypress', '.cypress', '.vite'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
